8626335706a6419f8f92fcdb3cc5629ebf033c33
ianverger <ian.verger@gmail.com>
2022-08-15 16:51:19 -0400

    break

--- Hash_map_LRU/lib/p01_int_set.rb
+++ Hash_map_LRU/lib/p01_int_set.rb
@@ -1,14 +1,25 @@
 class MaxIntSet
+  attr_reader :max, :store
+
   def initialize(max)
+    @max = max
+    @store = Array.new(@max, false)
   end
 
   def insert(num)
+    if num <= @max && num >= 0
+      @store[num] = true
+    else
+      raise 'Out of bounds'
+    end
   end
 
   def remove(num)
+    @store[num] = false
   end
 
   def include?(num)
+    return @store[num]
   end
 
   private
@@ -27,18 +38,33 @@ class IntSet
   end
 
   def insert(num)
+    mod = num % num_buckets
+    @store[mod] << num
   end
 
   def remove(num)
+    if include?(num)
+        @store.each do |bucket|
+            if bucket.include?(num)
+              bucket.delete(num)
+            end
+        end
+    end
   end
 
   def include?(num)
+    @store.each do |bucket|
+      return true if bucket.include?(num)
+    end
+
+    false
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    mod = num % num_buckets
+    @store[mod]
   end
 
   def num_buckets
@@ -55,18 +81,28 @@ class ResizingIntSet
   end
 
   def insert(num)
+    return false if include?(num)
+    self[num] << num
+    @count += 1
+    resize! if self.num_buckets < @count
   end
 
   def remove(num)
+    if self.include?(num)
+      self[num].delete(num)
+      @count -= 1
+    end
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    mod = num % num_buckets
+    @store[mod]
   end
 
   def num_buckets
@@ -74,5 +110,10 @@ class ResizingIntSet
   end
 
   def resize!
+    old_store = @store
+    @count = 0
+    @store = Array.new(num_buckets * 2) { Array.new }
+
+    old_store.flatten.each { |num| insert(num) } 
   end
 end
